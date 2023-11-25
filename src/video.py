bd9a69c74978ae65c1ae11eea284b1ebb438cdaf
dhlee <jsleeg7279@naver.com>
2023-11-25 18:53:46 +0000

    Blur video

--- src/video.py
+++ src/video.py
@@ -7,6 +7,10 @@ from flask import Blueprint, request, jsonify
 from app import s3
 from src.db.room import Room
 
+from furiosa.runtime.sync import create_runner
+from utils.preprocess import *
+from utils.postprocess import *
+
 router = Blueprint('video', __name__, url_prefix='/video')
 
 
@@ -95,20 +99,26 @@ def video_convert(file_name):
         (width, height) = (int(video.get(cv2.CAP_PROP_FRAME_WIDTH)), int(video.get(cv2.CAP_PROP_FRAME_HEIGHT)))
         fps = video.get(cv2.CAP_PROP_FPS)
         video_no_audio_url = f'tmp/convert-{file_name}'
-        out = cv2.VideoWriter(video_no_audio_url, fourcc, fps, (width, height))
-        cnt = 0
-        while True:
-            ret, frame = video.read()
-            if ret:
-                # 무언가 frame 전처리 여기서.
-                # 일단 임시로 반전 효과 넣음.
-                cnt += 1
-                frame = 255 - frame
-                out.write(frame)
-            else:
-                break
-        video.release()
-        out.release()
+
+        with create_runner("yolov7_i8_2pe.enf", worker_num=8) as runner:
+            out = cv2.VideoWriter(video_no_audio_url, fourcc, fps, (width, height))
+            cnt = 0
+            while True:
+                ret, frame = video.read()
+                if ret:
+
+                    image_tensor, preproc_params = preproc(frame)
+                    output = runner.run(image_tensor)
+                    predictions = postproc(output, 0.65, 0.35)
+                    predictions = predictions[0]
+                    bboxed_img = draw_bbox(frame, predictions, preproc_params)
+                    cnt += 1
+                    # frame = 255 - frame
+                    out.write(bboxed_img)
+                else:
+                    break
+            video.release()
+            out.release()
 
         # s3에 convert 한 비디오 업로드
         s3.upload_fileobj(open(video_no_audio_url, 'rb'), 'furiosa-video', f'convert/{file_name}')
