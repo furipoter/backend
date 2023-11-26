b2f97e9abb6271b58fe522d16ee70fffa4668cae
dhlee <jsleeg7279@naver.com>
2023-11-26 03:48:42 +0000

    commit

--- src/video.py
+++ src/video.py
@@ -11,6 +11,7 @@ from furiosa.runtime.sync import create_runner
 from utils.preprocess import *
 from utils.postprocess import *
 import time
+import requests
 
 
 router = Blueprint('video', __name__, url_prefix='/video')
@@ -90,18 +91,25 @@ def video_upload():
         s3.download_file('furiosa-video', f'upload/{file_name}', download_url)
         video = cv2.VideoCapture(download_url)
         # 비디오 저장
-        fourcc = cv2.VideoWriter_fourcc(*'XVID')
+        fourcc = cv2.VideoWriter_fourcc(*'MP4V')
         (width, height) = (int(video.get(cv2.CAP_PROP_FRAME_WIDTH)), int(video.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+        new_width, new_height = width, height
+        if width > height and width >= 720:
+            new_width = 720
+            new_height = int((height * new_width) / width)
+        if height >=width and height >= 720:
+            new_height = 720
+            new_width = int((width * new_height) / height)
         fps = video.get(cv2.CAP_PROP_FPS)
         video_no_audio_url = f'tmp/convert-{file_name}'
 
         with create_runner("yolov7_i8_2pe.enf", worker_num=8) as runner:
-            out = cv2.VideoWriter(video_no_audio_url, fourcc, fps, (width, height))
+            out = cv2.VideoWriter(video_no_audio_url, fourcc, fps, (new_width, new_height))
             cnt = 0
             while True:
-                ret, frame = video.read()
+                ret, _frame = video.read()
                 if ret:
-
+                    frame = cv2.resize(_frame, dsize=(new_width, new_height), interpolation=cv2.INTER_LINEAR)
                     image_tensor, preproc_params = preproc(frame)
                     output = runner.run(image_tensor)
                     predictions = postproc(output, 0.45, 0.35)
@@ -116,15 +124,20 @@ def video_upload():
             video.release()
             out.release()
             print(time.time() - start_time)
+
         # s3에 convert 한 비디오 업로드
         s3.upload_fileobj(open(video_no_audio_url, 'rb'), 'furiosa-video', f'convert/{file_name}')
         convert_url = f'https://furiosa-video.s3.ap-northeast-2.amazonaws.com/convert/{file_name}'
 
+        url = 'http://3.36.4.183:5003/api/video/mp4/' + file_name
+        r = requests.get(url)
+        ffmpeg_url = r.json()['ffmpeg_url']
         # update_room_number(file_name)
         return jsonify({
             'message': 'Video converted successfully',
             'convert_url': convert_url,
             'upload_url': upload_url,
+            'ffmpeg_url': ffmpeg_url,
         })
     except Exception as e:
         return jsonify({
